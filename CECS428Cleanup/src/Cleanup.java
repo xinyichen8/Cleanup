9ab750eca9577ee3c6fece47b1baae39c72d255c
Xinyi <xinyichen8@gmail.com>
2019-03-27 20:17:00 -0700

    finalize code

--- CECS428Cleanup/src/Cleanup.java
+++ CECS428Cleanup/src/Cleanup.java
@@ -6,8 +6,10 @@ public class Cleanup
 	public static void main (String args[]) throws IOException
 	{
 		File f = new File("points.txt");
+		Random rand = new Random();
 		ArrayList<String> data = new ArrayList<>();
 		ArrayList<Point>points = new ArrayList<>();
+				
 		try 
 		{
 			String line;
@@ -30,42 +32,61 @@ public class Cleanup
 		}
 		
 		//calculating distance
-		HashSet<Point> used = new HashSet<>();
-		
-		
-	}
-	
-	public static void quickSort(ArrayList<Double> sensors, int low, int high) 
-	{
-		if(low<high)
+		HashMap<Point,HashMap<Double,Point>> pointDis = new HashMap<>();// binary heap? map point with distance
+		HashMap<Point,PriorityQueue<Double>> pointShort = new HashMap<>();
+		for(int i = 0; i<points.size(); i++)
 		{
-			int pet = petition(sensors,low,high);
-			
-			quickSort(sensors,low,pet-1);
-			quickSort(sensors,pet+1,high);
-		}
-	}
-	
-	private static int petition(ArrayList<Double> unsorted, int low, int high) 
-	{
-		double pivot = unsorted.get(high);
-		int i = low-1;
-		for(int j= low; j<high; j++)
-		{
-			if (unsorted.get(j)<=pivot)
+			HashMap<Double,Point> distance = new HashMap<>();
+			PriorityQueue<Double> dis = new PriorityQueue<>();
+			for(int j = 0; j<points.size();j++)
 			{
-				i++;
-				double temp = unsorted.get(i);
-				unsorted.set(i, unsorted.get(j));
-				unsorted.set(j, temp);
+				distance.put(calculateDistance(points.get(i),points.get(j)),points.get(j));
+				dis.add(calculateDistance(points.get(i),points.get(j)));
+			}
+			pointDis.put(points.get(i), distance);
+			pointShort.put(points.get(i), dis);
+		}
+		
+		ArrayList<Point> route = new ArrayList<>();
+		HashSet<Point> used = new HashSet<>();
+		Point newp = points.get(rand.nextInt(points.size()));
+		route.add(newp);
+		used.add(newp);
+		while(used.size()<points.size())
+		{
+			double sd = pointShort.get(newp).poll();
+			Point p = pointDis.get(newp).get(sd); //get the point with shortest distance
+			
+			while(used.contains(p))
+			{
+				sd = pointShort.get(newp).poll();
+				p = pointDis.get(newp).get(sd);
+			}
+			if(!used.contains(p))
+			{
+				used.add(p);
+				route.add(p);
+				newp = p;
 			}
 		}
+		PrintWriter writer = new PrintWriter("path.txt");
+		for (Point p: route)
+		{
+			writer.println(p.getloc(1)+","+p.getloc(2)+","+p.getloc(3)+","+p.getloc(4));
+		}
 		
-		double temp = unsorted.get(i+1);
-		unsorted.set(i+1,unsorted.get(high));
-		unsorted.set(high, temp);
-		
-		return i+1;
+	    writer.close();
 	}
+	
+	
+	public static Double calculateDistance(Point a, Point b)
+	{
+		double x = Math.pow(a.getloc(1)-b.getloc(1), 2);
+		double y = Math.pow(a.getloc(2)-b.getloc(2), 2);
+		double z = Math.pow(a.getloc(3)-a.getloc(3), 2);
+		double w = Math.pow(a.getloc(4)-b.getloc(4), 2);
+		return Math.sqrt(x+y+z+w);
+	}
+	
 	
 }
